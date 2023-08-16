3287175dfcf6d753868972cd95d75a38fd0b766d
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-08-16 14:22:23 -0400

    fixed the route issue by killing port 3001 and restarting the server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require("./Develop/routes");
 const sequelize = require("./Develop/config/connection");
 
 const app = express();
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
